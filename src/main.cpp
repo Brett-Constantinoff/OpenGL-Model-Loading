6247b6570063c37df61e7299a2afb9680ce4a62a
Brett Constantinoff <constantinoffbrett@gmail.com>
2022-01-13 16:19:44 -0700

    added boiler plate to open a window

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,50 @@
+#define GL_SILENCE_DEPRECATION
+
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
 
 #include <iostream>
 
 int main(){
-    std::cout << "Hello World" << std::endl;
+    /* INIT GLFW */
+    glfwInit();
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+#ifdef __APPLE__
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+#endif
+
+    /* WINDOW INIT */
+    const unsigned int width = 800, height = 600;
+    GLFWwindow *win = glfwCreateWindow(width, height, "Hello World", NULL, NULL);
+    if(!win){
+        std::cerr << "Error creating OpenGL window" << std::endl;
+        exit(EXIT_FAILURE);
+    }
+    glfwMakeContextCurrent(win);
+    glViewport(0, 0, width, height);
+
+    /* GLEW INIT */
+    glewExperimental = true;
+    if(glewInit() != GLEW_OK){
+        std::cerr << "ERROR INITIALIZING GLEW" << std::endl;
+        exit(EXIT_FAILURE);
+    }
+
+    /* MAIN RENDER LOOP */
+    while(!glfwWindowShouldClose(win)){
+
+        glClearColor(1.0f, 1.0f, 1.0f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        glfwSwapBuffers(win); //swap front and back buffer
+        glfwPollEvents();
+    }
+
     return 0;
+
+
+
 }
